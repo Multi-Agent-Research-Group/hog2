5d32a5f21d3dd0a3f6703bf36a196c66a2421abb
Thayne Walker <thawalke@cs.du.edu>
2016-07-13 18:35:14 -0600

    Fix fgeq and fleq

--- utils/FPUtil.h
+++ utils/FPUtil.h
@@ -34,12 +34,12 @@
 // Floating point comparisons 
 static const double TOLERANCE = 0.000001;    // floating point tolerance
 
-inline bool fleq(double a, double b) { return (a <= b - TOLERANCE); }
-inline bool fgeq(double a, double b) { return (a >= b + TOLERANCE); }
 inline bool fless(double a, double b) { return (a < b - TOLERANCE); }
 inline bool fgreater(double a, double b) { return (a > b + TOLERANCE); }
 inline bool fequal(double a, double b)
 { return (a >= b - TOLERANCE) && (a <= b+TOLERANCE); }
+inline bool fleq(double a, double b) { return fless(a,b)||fequal(a,b); }
+inline bool fgeq(double a, double b) { return fgreater(a,b)||fequal(a,b); }
 
 inline double min(double a, double b) { return fless(a, b)?a:b; }
 inline double max(double a, double b) { return fless(a, b)?b:a; }
