4af6da9b36c3f0e4103ba4922797c4f15a90de72
thayne <kikansenkyoshi@hotmail.com>
2017-03-29 01:54:54 -0600

    Adding ICTS node

--- apps/icts/Driver.cpp
+++ apps/icts/Driver.cpp
@@ -83,16 +83,19 @@ std::ostream& operator << (std::ostream& ss, Node const* n){
 
 MapEnvironment* Node::env=nullptr;
 
-bool LimitedDFS(xyLoc const& start, xyLoc const& end, DAG& dag, MDD& mdd, int depth){
-  //std::cout << start << " " << depth << "\n";
-  if(depth==0)
+bool LimitedDFS(xyLoc const& start, xyLoc const& end, DAG& dag, MDD& mdd, int depth, int maxDepth){
+  //std::cout << start << " g:" << (maxDepth-depth) << " h:" << Node::env->HCost(start,end) << " f:" << ((maxDepth-depth)+Node::env->HCost(start,end)) << "\n";
+  if(depth<0 ||
+      (maxDepth-depth)+Node::env->HCost(start,end)>maxDepth){ // Note - this only works for a perfect heuristic.
+    //std::cout << "pruned\n";
     return false;
+  }
 
   if(Node::env->GoalTest(end,start)){
     Node n(start,depth);
     uint64_t hash(n.Hash());
     dag[hash]=n;
-    std::unordered_set<Node*>& s=mdd[depth-1];
+    std::unordered_set<Node*>& s=mdd[maxDepth-(depth)];
     s.insert(&dag[hash]);
     //std::cout << "found " << start << "\n";
     return true;
@@ -102,12 +105,12 @@ bool LimitedDFS(xyLoc const& start, xyLoc const& end, DAG& dag, MDD& mdd, int de
   Node::env->GetSuccessors(start,successors);
   bool result(false);
   for(auto const& node: successors){
-    if(LimitedDFS(node,end,dag,mdd,depth-1)){
+    if(LimitedDFS(node,end,dag,mdd,depth-1,maxDepth)){
       Node n(start,depth);
       uint64_t hash(n.Hash());
       if(dag.find(hash)==dag.end()){
         dag[hash]=n;
-        std::unordered_set<Node*>& s=mdd[depth-1];
+        std::unordered_set<Node*>& s=mdd[maxDepth-(depth)];
         s.insert(&dag[hash]);
       }else if(dag[hash].optimal){
         return true; // Already found a solution from search at this depth
@@ -128,6 +131,61 @@ bool LimitedDFS(xyLoc const& start, xyLoc const& end, DAG& dag, MDD& mdd, int de
   return result;
 }
 
+void GetMDD(xyLoc const& start, xyLoc const& end, MDD& mdd, int depth){
+  DAG dag;
+  mdd.resize(depth+1);
+  LimitedDFS(start,end,dag,mdd,depth,depth);
+  for(auto const& a: mdd){
+    for(auto const& n: a){
+      std::cout << *n;
+    }
+    std::cout << "\n";
+  }
+}
+
+struct ICTSNode{
+  ICTSNode(int agents):mdd(agents){}
+  std::vector<MDD> mdd;
+  // Returns true and sets a1, a2 on conflict
+  bool CheckConflicts(int** a1, int** a2){
+    // Check all pairs
+    for(int i(0); i<mdd.size(); ++i){
+      for(int j(i+1); j<mdd.size(); ++j){
+        // Check all times
+        for(int t(0); t<std::min(mdd[i].size(),mdd[j].size()); ++t){
+          // Check for node and edge conflicts
+// Create a subroutine that does the following:
+//2. If I now have no more parents, erase children's pointers to me, then erase myself; recurse downward
+//3. If the parent has no children, erase it's parent pointer and it's parent's child pointer; recurse upward
+// Finally, check for severed connectivity by checking across a time slice. If all nodes have no parents or children, then this ICTS node is rendered infeasible
+          for(auto const& m: mdd[i][t]){
+            for(auto const& n: mdd[j][t]){
+              if(m == n){
+                //TODO Use iterator and "erase in place"
+                //1. Erase parent's pointers to me and my pointers to parents
+                //2. Erase children's pointers to me and my pointers to children
+                //3. Call eraser subroutine
+              }
+              if(t!=0){ // Check edges
+                for(auto& mp: m->parents){
+                  for(auto& np: n->parents){
+                    if(mp->n==n->n || np->n==m->n){ // Same edge traversal
+                      //TODO Use iterator and "erase in place"
+                      //1. Erase parent's pointer to me and my pointer to parent
+                      //2. Call eraser subroutine
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return true;
+  }
+};
+
 int main(){
   MapEnvironment env(new Map(8,8));
   env.SetFiveConnected();
@@ -135,17 +193,15 @@ int main(){
 
   xyLoc s(1,1);
   xyLoc e(5,5);
-  int sdepth(Node::env->HCost(s,e)+1);
-  DAG dag;
-  MDD mdd(sdepth);
-  LimitedDFS(s,e,dag,mdd,sdepth);
+  int sdepth(Node::env->HCost(s,e));
+  int agents(1);
+  ICTSNode n(agents);
+  GetMDD(s,e,n.mdd[0],sdepth);
+  MDD mdd2;
+  GetMDD(s,e,mdd2,sdepth+1);
+  MDD mdd3;
+  GetMDD(s,e,mdd3,sdepth+2);
 
-  for(auto const& a: mdd){
-    for(auto const& n: a){
-      std::cout << *n;
-    }
-    std::cout << "\n";
-  }
   //std::cout << &dag[Node(s,sdepth).Hash()];
   //std::cout << dag[Node(s,sdepth).Hash()].successors.size();
   return 0;
